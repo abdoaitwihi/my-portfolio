176c293076077b59286aa2f6be2e4d4c237f19fb
abdo <abdoaitwihi@gmail.com>
2020-10-04 13:11:21 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-portfolio';
+  person: Person = {
+    name: 'abderrahmane',
+    email: 'abodaitwihi@gmail.com',
+    phone: '0651080175',
+  };
 }
